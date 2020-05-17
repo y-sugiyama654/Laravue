e120bc21fab1c4f6f0898f806f14492f38a92462
Yuta <y.sugiyama54@gmail.com>
2020-05-17 21:20:51 +0900

    データ取得時のテスト追加

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/contacts/{contact}', 'ContactsController@show');
 Route::post('/contacts', 'ContactsController@store');
