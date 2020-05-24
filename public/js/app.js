771b51d5fe0dfe69775b0866e3290724c65d5dc1
Yuta <y.sugiyama54@gmail.com>
2020-05-24 15:07:48 +0900

    homeページの作成1

--- public/js/app.js
+++ public/js/app.js
@@ -1917,6 +1917,17 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "App"
 });
@@ -37499,13 +37510,202 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c(
-    "div",
-    [_c("h1", [_vm._v("Hello")]), _vm._v(" "), _c("router-view")],
-    1
-  )
+  return _c("div", { staticClass: "h-screen bg-white" }, [
+    _c("div", { staticClass: "flex" }, [
+      _c(
+        "div",
+        {
+          staticClass:
+            "pl-6 pt-3 bg-blue-800 w-1/6 h-screen border-r-2 border-gray-400"
+        },
+        [
+          _c("router-link", { attrs: { to: "/" } }, [
+            _c(
+              "svg",
+              {
+                staticStyle: {
+                  cursor: "pointer",
+                  "max-width": "100%",
+                  "max-height": "48px"
+                },
+                attrs: {
+                  xmlns: "http://www.w3.org/2000/svg",
+                  "xmlns:xlink": "http://www.w3.org/1999/xlink",
+                  version: "1.1",
+                  width: "181px",
+                  viewBox: "-0.5 -0.5 181 48",
+                  content:
+                    '<mxfile host="app.diagrams.net" modified="2020-05-23T09:29:32.051Z" agent="5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36" etag="HE6jM4ghji1ju8ChEeZ-" version="13.1.3" type="google"><diagram id="eoWUthKHQIw06nZ4uOaG" name="Page-1">jZNNb8IwDIZ/TY+TaAvdOA72pWmckBjaZcoa02akcQkplP36uY1L6aZJE1JJHtuJ7bwO4nlRP1pR5guUoINoJOsgvguiaDpO6NuAkwdJOPYgs0p6FPZgqb6A4YhppSTsB44OUTtVDmGKxkDqBkxYi8eh2wb18NZSZPALLFOhf9NXJV3u6U103fMnUFne3RwmU28pROfMlexzIfF4geL7IJ5bROdXRT0H3fSu64uPe/jDek7MgnH/CVi56W69eNss6/UzfOrt+2r7fsVlHISuuOAgSjSdN9sgHUtZuxO3ItlV2Bmu9u1D3ZLDeFTWvbF9CY12EBFEMSTNb+C3Eenw4BVYKYy4dEqy5v9FWLGi/DgzKtEn563c3XOekYO64bkrNIGQlhYoX/HROoxoLyqHvoLWLLTKDK1T6iRQ6rMDWKdIA7dsKJSUTfCsRGVcq6jJLJjcNd1QWs+5YoOmcdo7i1v4AS1WRoLkBLjrdA3Ufz5neBYJDRdgAc6eyIUDEpYVz9WEt8depOENs/xCoDEzwXORnQ/upUMLVk+37VXa2i5GPb7/Bg==</diagram></mxfile>',
+                  onclick:
+                    "(function(svg){var src=window.event.target||window.event.srcElement;while (src!=null&&src.nodeName.toLowerCase()!='a'){src=src.parentNode;}if(src==null){if(svg.wnd!=null&&!svg.wnd.closed){svg.wnd.focus();}else{var r=function(evt){if(evt.data=='ready'&&evt.source==svg.wnd){svg.wnd.postMessage(decodeURIComponent(svg.getAttribute('content')),'*');window.removeEventListener('message',r);}};window.addEventListener('message',r);svg.wnd=window.open('https://app.diagrams.net/?client=1&lightbox=1&edit=_blank');}}})(this);"
+                }
+              },
+              [
+                _c("defs"),
+                _c("g", [
+                  _c("rect", {
+                    attrs: {
+                      x: "0",
+                      y: "9",
+                      width: "180",
+                      height: "30",
+                      fill: "none",
+                      stroke: "none",
+                      "pointer-events": "all"
+                    }
+                  }),
+                  _c("g", { attrs: { transform: "translate(-0.5 -0.5)" } }, [
+                    _c(
+                      "switch",
+                      [
+                        _c(
+                          "foreignObject",
+                          {
+                            staticStyle: {
+                              overflow: "visible",
+                              "text-align": "left"
+                            },
+                            attrs: {
+                              "pointer-events": "none",
+                              width: "100%",
+                              height: "100%",
+                              requiredFeatures:
+                                "http://www.w3.org/TR/SVG11/feature#Extensibility"
+                            }
+                          },
+                          [
+                            _c(
+                              "div",
+                              {
+                                staticStyle: {
+                                  display: "flex",
+                                  "align-items": "unsafe center",
+                                  "justify-content": "unsafe center",
+                                  width: "1px",
+                                  height: "1px",
+                                  "padding-top": "24px",
+                                  "margin-left": "90px"
+                                },
+                                attrs: { xmlns: "http://www.w3.org/1999/xhtml" }
+                              },
+                              [
+                                _c(
+                                  "div",
+                                  {
+                                    staticStyle: {
+                                      "box-sizing": "border-box",
+                                      "font-size": "0",
+                                      "text-align": "center"
+                                    }
+                                  },
+                                  [
+                                    _c(
+                                      "div",
+                                      {
+                                        staticStyle: {
+                                          display: "inline-block",
+                                          "font-size": "12px",
+                                          "font-family": "Helvetica",
+                                          color: "#000000",
+                                          "line-height": "1.2",
+                                          "pointer-events": "all",
+                                          "white-space": "nowrap"
+                                        }
+                                      },
+                                      [
+                                        _c(
+                                          "font",
+                                          {
+                                            staticStyle: {
+                                              "font-size": "40px"
+                                            },
+                                            attrs: {
+                                              color: "#e6e6e6",
+                                              face: "Verdana"
+                                            }
+                                          },
+                                          [_vm._v("LaraVue")]
+                                        )
+                                      ],
+                                      1
+                                    )
+                                  ]
+                                )
+                              ]
+                            )
+                          ]
+                        ),
+                        _c(
+                          "text",
+                          {
+                            attrs: {
+                              x: "90",
+                              y: "28",
+                              fill: "#000000",
+                              "font-family": "Helvetica",
+                              "font-size": "12px",
+                              "text-anchor": "middle"
+                            }
+                          },
+                          [_vm._v("LaraVue")]
+                        )
+                      ],
+                      1
+                    )
+                  ])
+                ]),
+                _c("switch", [
+                  _c("g", {
+                    attrs: {
+                      requiredFeatures:
+                        "http://www.w3.org/TR/SVG11/feature#Extensibility"
+                    }
+                  }),
+                  _c(
+                    "a",
+                    {
+                      attrs: {
+                        transform: "translate(0,-5)",
+                        "xlink:href":
+                          "https://desk.draw.io/support/solutions/articles/16000042487",
+                        target: "_blank"
+                      }
+                    },
+                    [
+                      _c(
+                        "text",
+                        {
+                          attrs: {
+                            "text-anchor": "middle",
+                            "font-size": "10px",
+                            x: "50%",
+                            y: "100%"
+                          }
+                        },
+                        [_vm._v("Viewer does not support full SVG 1.1")]
+                      )
+                    ]
+                  )
+                ])
+              ]
+            )
+          ])
+        ],
+        1
+      ),
+      _vm._v(" "),
+      _vm._m(0)
+    ])
+  ])
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", [_c("div"), _vm._v(" "), _c("div")])
+  }
+]
 render._withStripped = true
 
 
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>
-        <h1>Hello</h1>
-        <router-view>
+    <div class="h-screen bg-white">
+        <div class="flex">
+            <div class="pl-6 pt-3 bg-blue-800 w-1/6 h-screen border-r-2 border-gray-400">
 
-        </router-view>
+                <router-link to="/">
+                    <!-- メインアイコン -->
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="181px" viewBox="-0.5 -0.5 181 48" content="&lt;mxfile host=&quot;app.diagrams.net&quot; modified=&quot;2020-05-23T09:29:32.051Z&quot; agent=&quot;5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36&quot; etag=&quot;HE6jM4ghji1ju8ChEeZ-&quot; version=&quot;13.1.3&quot; type=&quot;google&quot;&gt;&lt;diagram id=&quot;eoWUthKHQIw06nZ4uOaG&quot; name=&quot;Page-1&quot;&gt;jZNNb8IwDIZ/TY+TaAvdOA72pWmckBjaZcoa02akcQkplP36uY1L6aZJE1JJHtuJ7bwO4nlRP1pR5guUoINoJOsgvguiaDpO6NuAkwdJOPYgs0p6FPZgqb6A4YhppSTsB44OUTtVDmGKxkDqBkxYi8eh2wb18NZSZPALLFOhf9NXJV3u6U103fMnUFne3RwmU28pROfMlexzIfF4geL7IJ5bROdXRT0H3fSu64uPe/jDek7MgnH/CVi56W69eNss6/UzfOrt+2r7fsVlHISuuOAgSjSdN9sgHUtZuxO3ItlV2Bmu9u1D3ZLDeFTWvbF9CY12EBFEMSTNb+C3Eenw4BVYKYy4dEqy5v9FWLGi/DgzKtEn563c3XOekYO64bkrNIGQlhYoX/HROoxoLyqHvoLWLLTKDK1T6iRQ6rMDWKdIA7dsKJSUTfCsRGVcq6jJLJjcNd1QWs+5YoOmcdo7i1v4AS1WRoLkBLjrdA3Ufz5neBYJDRdgAc6eyIUDEpYVz9WEt8depOENs/xCoDEzwXORnQ/upUMLVk+37VXa2i5GPb7/Bg==&lt;/diagram&gt;&lt;/mxfile&gt;" onclick="(function(svg){var src=window.event.target||window.event.srcElement;while (src!=null&amp;&amp;src.nodeName.toLowerCase()!='a'){src=src.parentNode;}if(src==null){if(svg.wnd!=null&amp;&amp;!svg.wnd.closed){svg.wnd.focus();}else{var r=function(evt){if(evt.data=='ready'&amp;&amp;evt.source==svg.wnd){svg.wnd.postMessage(decodeURIComponent(svg.getAttribute('content')),'*');window.removeEventListener('message',r);}};window.addEventListener('message',r);svg.wnd=window.open('https://app.diagrams.net/?client=1&amp;lightbox=1&amp;edit=_blank');}}})(this);" style="cursor:pointer;max-width:100%;max-height:48px;"><defs/><g><rect x="0" y="9" width="180" height="30" fill="none" stroke="none" pointer-events="all"/><g transform="translate(-0.5 -0.5)"><switch><foreignObject style="overflow: visible; text-align: left;" pointer-events="none" width="100%" height="100%" requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"><div xmlns="http://www.w3.org/1999/xhtml" style="display: flex; align-items: unsafe center; justify-content: unsafe center; width: 1px; height: 1px; padding-top: 24px; margin-left: 90px;"><div style="box-sizing: border-box; font-size: 0; text-align: center; "><div style="display: inline-block; font-size: 12px; font-family: Helvetica; color: #000000; line-height: 1.2; pointer-events: all; white-space: nowrap; "><font style="font-size: 40px" color="#e6e6e6" face="Verdana">LaraVue</font></div></div></div></foreignObject><text x="90" y="28" fill="#000000" font-family="Helvetica" font-size="12px" text-anchor="middle">LaraVue</text></switch></g></g><switch><g requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"/><a transform="translate(0,-5)" xlink:href="https://desk.draw.io/support/solutions/articles/16000042487" target="_blank"><text text-anchor="middle" font-size="10px" x="50%" y="100%">Viewer does not support full SVG 1.1</text></a></switch></svg>
+                </router-link>
+
+            </div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+
     </div>
 </template>
 
