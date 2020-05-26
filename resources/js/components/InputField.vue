c690db3231359761a0ff85aa84a7805f2a12efe9
Yuta <y.sugiyama54@gmail.com>
2020-05-26 22:09:31 +0900

    Contact.createページにinputFieldコンポーネントの導入

---
+++ resources/js/components/InputField.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="relative pt-4">
+        <label :for="name" class="uppercase text-blue-500 pt-2 text-xs font-bold absolute">{{ label }}</label>
+        <input type="text" :id="name" class="pt-8 text-gray-600 w-full border-b pb-2 focus:outline-none focus:border-blue-400" :placeholder="placeholder" v-model="value" @input="updateField()">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "InputField",
+
+        props: [
+            'name', 'label', 'placeholder'
+        ],
+
+        data: function() {
+            return {
+                value: ''
+            }
+        },
+
+        methods: {
+            updateField: function() {
+                this.$emit('update:field', this.value)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- resources/js/views/ContactsCreate.vue
+++ resources/js/views/ContactsCreate.vue
@@ -1,11 +1,12 @@
 <template>
     <div>
         <form action="">
-            <div class="relative">
-                <label for="name" class="uppercase text-blue-500 pt-2 text-xs font-bold absolute">Contact Name</label>
-                <input type="text" id="name" class="pt-8 text-gray-600 w-full border-b pb-2 focus:outline-none focus:border-blue-400" placeholder="Contact Name">
-            </div>
-            
+
+            <InputField name="name" label="Contact Name" placeholder="Contact Name" @update:field="form.name = $event" />
+            <InputField name="email" label="Contact Email" placeholder="Contact Email" @update:field="form.email = $event" />
+            <InputField name="company" label="Company" placeholder="Company" @update:field="form.company = $event" />
+            <InputField name="birthday" label="Birthday" placeholder="MM/DD/YYYY" @update:field="form.birthday = $event" />
+
             <div class="flex justify-end py-2">
                 <button class="border py-2 px-4 rounded text-red-700 mr-4 hover:bg-gray-200">Cancel</button>
                 <button class="bg-blue-500 py-2 px-4 rounded text-white hover:bg-blue-400">Add New Contact</button>
@@ -16,8 +17,22 @@
 </template>
 
 <script>
+    import InputField from '../components/InputField';
     export default {
-        name: "ContactsCreate"
+        name: "ContactsCreate",
+        components: {
+            InputField,
+        },
+        data: function() {
+            return {
+                form: {
+                    'name': '',
+                    'email': '',
+                    'company': '',
+                    'birthday': '',
+                }
+            }
+        }
     }
 </script>
 
